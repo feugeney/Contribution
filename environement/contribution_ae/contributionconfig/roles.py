05aeee2f638e33d13aeae6a1a62d635d2d2c2a0d
feugeney <feugeneyomalo@gmail.com>
2025-07-17 11:02:08 +0000

    Delete environement directory

--- environement/contribution_ae/contributionconfig/roles.py
+++
@@ -1,1 +0,0 @@
-from rolepermissions.roles import AbstractUserRole
--- environement/frontend/supabase/migrations/20250714115136-45e411f6-6375-4c48-bddc-4e4c370adf70.sql
+++
@@ -1,7 +0,0 @@
--- Ajouter une colonne pour stocker les fichiers de courriers électroniques
-ALTER TABLE public.courriers 
-ADD COLUMN fichier_courrier_url TEXT;
-
--- Mettre à jour les commentaires des colonnes existantes
-COMMENT ON COLUMN public.courriers.organisation_id IS 'Organisation internationale pour laquelle on paie la cotisation';
-COMMENT ON COLUMN public.courriers.fichier_courrier_url IS 'URL du fichier du courrier électronique joint';
